386e3853a88c496ccdca921acaa18cbda1552f2c
Carlos <carlos_ds730@yahoo.es>
2016-06-01 12:19:48 -0400

    master's settings are ready to nginx

--- Football/settings.py
+++ Football/settings.py
@@ -112,7 +112,7 @@ MEDIA_URL = '/media/'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 THUMBNAIL_ENGINE = 'sorl.thumbnail.engines.wand_engine.Engine'
 
